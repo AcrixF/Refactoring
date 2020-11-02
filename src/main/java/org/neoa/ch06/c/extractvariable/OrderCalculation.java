863b42cfd51d299bb7e05ee3978aa6210ab9553d
Cristian <acrixf@gmail.com>
2020-11-02 15:26:53 -0600

    remove commentary / extract to variable Shipping calculate

--- src/main/java/org/neoa/ch06/c/extractvariable/OrderCalculation.java
+++ src/main/java/org/neoa/ch06/c/extractvariable/OrderCalculation.java
@@ -3,11 +3,9 @@ package org.neoa.ch06.c.extractvariable;
 public class OrderCalculation {
 
     public double price(Order order) {
-        // price is base price - quantity discount + shipping
         double basePrice = order.getQuantity() * order.getItemPrice();
         double quantityDiscount = Math.max(0, order.getQuantity() - 500) * order.getItemPrice() * 0.05;
-        return basePrice -
-                quantityDiscount +
-                Math.min(basePrice * 0.01, 100);
+        double shipping = Math.min(basePrice * 0.01, 100);
+        return basePrice - quantityDiscount + shipping;
     }
 }
