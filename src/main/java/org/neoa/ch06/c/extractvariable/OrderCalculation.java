534f6600013459711bb12ac9fd5e1a484f373453
Cristian <acrixf@gmail.com>
2020-11-02 14:53:21 -0600

    replace basePrice where it is used

--- src/main/java/org/neoa/ch06/c/extractvariable/OrderCalculation.java
+++ src/main/java/org/neoa/ch06/c/extractvariable/OrderCalculation.java
@@ -5,8 +5,8 @@ public class OrderCalculation {
     public double price(Order order) {
         // price is base price - quantity discount + shipping
         double basePrice = order.getQuantity() * order.getItemPrice();
-        return order.getQuantity() * order.getItemPrice() -
+        return basePrice -
                 Math.max(0, order.getQuantity() - 500) * order.getItemPrice() * 0.05 +
-                Math.min(order.getQuantity() * order.getItemPrice() * 0.01, 100);
+                Math.min(basePrice * 0.01, 100);
     }
 }
