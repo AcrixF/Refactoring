189d827b8575e3e19ccbf3bdebee5d7d921f14f4
Cristian <acrixf@gmail.com>
2020-11-02 15:39:43 -0600

    extract to method variables

--- src/main/java/org/neoa/ch06/c/extractvariable/OrderCalculation.java
+++ src/main/java/org/neoa/ch06/c/extractvariable/OrderCalculation.java
@@ -3,9 +3,18 @@ package org.neoa.ch06.c.extractvariable;
 public class OrderCalculation {
 
     public double price(Order order) {
-        double basePrice = order.getQuantity() * order.getItemPrice();
-        double quantityDiscount = Math.max(0, order.getQuantity() - 500) * order.getItemPrice() * 0.05;
-        double shipping = Math.min(basePrice * 0.01, 100);
-        return basePrice - quantityDiscount + shipping;
+        return basePrice(order) - quantityDiscount(order) + shipping(order);
+    }
+
+    private double shipping(Order order) {
+        return Math.min(basePrice(order) * 0.01, 100);
+    }
+
+    private double quantityDiscount(Order order) {
+        return Math.max(0, order.getQuantity() - 500) * order.getItemPrice() * 0.05;
+    }
+
+    private double basePrice(Order order) {
+        return order.getQuantity() * order.getItemPrice();
     }
 }
