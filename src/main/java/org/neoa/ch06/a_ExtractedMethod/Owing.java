0e13e3bc522003219bbe7de98c198710fb4a9eba
Cristian <cristian.flores@deepsee.ai>
2020-11-02 00:32:40 -0600

    add Extracted Method for addBanner and addDetails

--- src/main/java/org/neoa/ch06/a_ExtractedMethod/Owing.java
+++ src/main/java/org/neoa/ch06/a_ExtractedMethod/Owing.java
@@ -8,9 +8,7 @@ public class Owing {
         String result = "";
         int outstanding = 0;
 
-        result += "*************************\n";
-        result += "***** Customer Owes *****\n";
-        result += "*************************\n";
+        result = addBanner(result);
 
         for (Order order: invoice.getOrders()) {
             outstanding += order.getAmount();
@@ -19,10 +17,22 @@ public class Owing {
         LocalDate today = Clock.today();
         invoice.setDueDate(today.plusDays(30));
 
+        result = addDetails(invoice, result, outstanding);
+
+        return result;
+    }
+
+    private String addDetails(Invoice invoice, String result, int outstanding) {
         result += "name: " + invoice.getCustomer() + "\n";
         result += "amount: " + outstanding + "\n";
         result += "due: " + invoice.getDueDate().toString() + "\n";
+        return result;
+    }
 
+    private String addBanner(String result) {
+        result += "*************************\n";
+        result += "***** Customer Owes *****\n";
+        result += "*************************\n";
         return result;
     }
 
