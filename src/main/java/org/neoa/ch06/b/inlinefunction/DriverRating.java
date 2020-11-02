95df8110535a89e46e9cc103dee9a14ed6d18b09
Cristian <acrixf@gmail.com>
2020-11-02 12:11:19 -0600

    refactor inline function - remove moreThanFiveLateDeliveries method

--- src/main/java/org/neoa/ch06/b/inlinefunction/DriverRating.java
+++ src/main/java/org/neoa/ch06/b/inlinefunction/DriverRating.java
@@ -3,11 +3,7 @@ package org.neoa.ch06.b.inlinefunction;
 public class DriverRating {
 
     public int rating(Driver driver) {
-        return moreThanFiveLateDeliveries(driver) ? 2 : 1;
-    }
-
-    private boolean moreThanFiveLateDeliveries(Driver driver) {
-        return driver.getNumberOfLateDeliveries() > 5;
+        return driver.getNumberOfLateDeliveries() > 5 ? 2 : 1;
     }
 
 }
