03ade764be4c288b5d2cf3e2bfbbed53ba7e13d6
Cristian <acrixf@gmail.com>
2020-11-02 12:28:17 -0600

    undo inline refactor

--- src/main/java/org/neoa/ch06/b/inlinefunction/DriverRating.java
+++ src/main/java/org/neoa/ch06/b/inlinefunction/DriverRating.java
@@ -3,7 +3,11 @@ package org.neoa.ch06.b.inlinefunction;
 public class DriverRating {
 
     public int rating(Driver driver) {
-        return driver.getNumberOfLateDeliveries() > 5 ? 2 : 1;
+        return moreThanFiveLateDeliveries(driver) ? 2 : 1;
+    }
+
+    private boolean moreThanFiveLateDeliveries(Driver dvr) {
+        return dvr.getNumberOfLateDeliveries() > 5;
     }
 
 }
