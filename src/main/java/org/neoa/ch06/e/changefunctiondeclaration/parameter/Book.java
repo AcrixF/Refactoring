af1e014967f41872ed26f4f71588e9307201d0d9
Cristian <cristian.flores@deepsee.ai>
2020-11-05 23:20:02 -0600

    add parameter to the new function

--- src/main/java/org/neoa/ch06/e/changefunctiondeclaration/parameter/Book.java
+++ src/main/java/org/neoa/ch06/e/changefunctiondeclaration/parameter/Book.java
@@ -13,10 +13,10 @@ public class Book {
     private List<Customer> reservations = new ArrayList<>();
 
     public void addReservation(Customer customer) {
-        zz_addReservation(customer);
+        zz_addReservation(customer, false);
     }
 
-    private void zz_addReservation(Customer customer) {
+    private void zz_addReservation(Customer customer, boolean isPriority) {
         if (!Objects.isNull(customer))
             this.reservations.add(customer);
     }
