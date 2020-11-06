49a2154c15653ad7e245035d4b9d0cdde4b87b71
Cristian <cristian.flores@deepsee.ai>
2020-11-05 22:25:52 -0600

    Renaming a Function Simple Mechanic

---
+++ src/test/java/org/neoa/ch06/e/changefunctiondeclaration/CircleTest.java
@@ -0,0 +1,25 @@
+package org.neoa.ch06.e.changefunctiondeclaration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CircleTest {
+
+    private Circle circle;
+
+    @BeforeEach
+    void setup() {
+        circle = new Circle();
+    }
+
+    @Test
+    void circumReturnsExpectedWhenNoErrorsOccurs() {
+
+        double circumference = circle.circum(3);
+
+        assertThat(circumference).isEqualTo(18.84955592153876);
+
+    }
+}
