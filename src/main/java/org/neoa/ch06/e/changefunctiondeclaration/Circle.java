7decf49075409045444575059fdc2fec48b94bfb
Cristian <cristian.flores@deepsee.ai>
2020-11-05 22:33:00 -0600

    extract function to the entire function body

--- src/main/java/org/neoa/ch06/e/changefunctiondeclaration/Circle.java
+++ src/main/java/org/neoa/ch06/e/changefunctiondeclaration/Circle.java
@@ -3,6 +3,10 @@ package org.neoa.ch06.e.changefunctiondeclaration;
 public class Circle {
 
     public double circum(double radius) {
+        return circumference(radius);
+    }
+
+    private double circumference(double radius) {
         return 2 * Math.PI * radius;
     }
 }
