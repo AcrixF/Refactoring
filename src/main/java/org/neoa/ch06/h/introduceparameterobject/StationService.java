a188b9a7c8409d372bd7fb1880ed776e8a19bc2a
Cristian <cristian.flores@deepsee.ai>
2020-11-16 22:36:51 -0600

    Remove old method reference

--- src/main/java/org/neoa/ch06/h/introduceparameterobject/StationService.java
+++ src/main/java/org/neoa/ch06/h/introduceparameterobject/StationService.java
@@ -4,11 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StationService {
-
-    public List<Reading> readingsOutsideRange(Station station, double min, double max) {
-        return readingsOutsideRange_(station, min, max, null);
-    }
-
+    
     public List<Reading> readingsOutsideRange_(Station station, double min, double max, NumberRange range) {
         return station.getReadings().stream()
                 .filter(reading -> reading.getTemperature() < min || reading.getTemperature() > max)
