243721754ebc845feb4ca0cc0c56692cc8ae62b4
Cristian <cristian.flores@deepsee.ai>
2020-11-16 22:44:27 -0600

    Remove the min parameter

--- src/main/java/org/neoa/ch06/h/introduceparameterobject/StationService.java
+++ src/main/java/org/neoa/ch06/h/introduceparameterobject/StationService.java
@@ -5,9 +5,13 @@ import java.util.stream.Collectors;
 
 public class StationService {
     
-    public List<Reading> readingsOutsideRange_(Station station, double min, double max, NumberRange range) {
+    public List<Reading> readingsOutsideRange(Station station, double min, double max, NumberRange range) {
+        return readingsOutsideRange_(station, max, range);
+    }
+
+    public List<Reading> readingsOutsideRange_(Station station, double max, NumberRange range) {
         return station.getReadings().stream()
-                .filter(reading -> reading.getTemperature() < min || reading.getTemperature() > max)
+                .filter(reading -> reading.getTemperature() < range.getMin() || reading.getTemperature() > max)
                 .collect(Collectors.toUnmodifiableList());
     }
 }
